92a17f4319bdcc69ce95e4864acb5b4b1abcd6b3
Nahuel Morales <nmorales@quadiontech.com>
2019-02-12 14:32:23 -0300

    #7: Se Agrega router con un esquema simple de las 3 pantallas

--- src/src/App.js
+++ src/src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from "react-router-dom";
+
 import Header from './components/Header/Header'
+import Home from './components/Home'
+import ItemsResult from './components/ItemsResult'
+import ItemDetail from './components/ItemDetail'
+
 import './App.scss';
 
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
         <div>
           <Header/>
+          <Route path="/" exact component={Home}/>
+          <Route path="/items" exact component={ItemsResult}/>
+          <Route path="/items/:id" component={ItemDetail}/>
         </div>  
+      </BrowserRouter>
     );
   }
 }
